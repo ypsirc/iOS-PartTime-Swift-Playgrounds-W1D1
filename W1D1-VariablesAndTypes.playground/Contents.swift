800dfad7e52fe7b7b1598cce1606987115ce3531
shawncrisp <ypsirc@gmail.com>
2016-05-10 20:46:19 -0400

    Answered Questions

--- W1D1-VariablesAndTypes.playground/Contents.swift
+++ W1D1-VariablesAndTypes.playground/Contents.swift
@@ -32,7 +32,7 @@ count = 10
 //: ### Challenge 1
 //:
 //: Now it's your turn. Change `greeting` to be a different string by using the assignment operator.
-
+greeting = "What time is it"
 
 
 //: *Note:* unlike math, where `x = 10` and `10 = x` are interchangeable, in programming the variable goes on the left of the `=` symbol, and the new value goes on the right.
@@ -41,10 +41,18 @@ count = 10
 //: ### Challenge 2
 //:
 //: We want to add 1 to whatever the value of `count` is now. We could manually look at the value in `count`, see that it is 10 and put 11 into `count` using the assignment operator. This works, but we can do it in a better way. We can set count to be whatever count is, plus 1. See if you can figure out the syntax for this.
+count = count + 1
+
+count += 1
+
+count
+
+print(count)
+
 
 
 //: *Note:* If you're ever unsure of the contents of a variable in a playground, write the variable on a line by itself and you will see its value printed to the right of it. Or you can use the `print` function like so: `print(variableName)`.
-
+print(count)
 
 //: ## Types
 //:
@@ -69,11 +77,11 @@ count
 //: What type are the variables `tipAmount`, and `isOpen`?
 
 var tipAmount = 6.01
-// tipAmount's type is:
+// tipAmount's type is: Double
 
 
 var isOpen = true
-// isOpen's type is:
+// isOpen's type is: Bool
 
 
 //: ## Type Inference
@@ -87,6 +95,7 @@ var height: Double = 10
 
 //: Since all our variables have types, even if we haven't specifically set them, we can't store any value in them... only values of the right type.
 //: Test this out. Try setting the value of `count` to be `height`:
+//count = height
 
 
 
@@ -102,6 +111,14 @@ var height: Double = 10
 //: Print out the uppercase and lowercase versions of the string below:
 
 var schoolName = "Lightouse Labs"
+
+print(schoolName.uppercaseString)
+
+print(schoolName.lowercaseString)
+
+schoolName
+
+
 
 
 
